f256a787c26b68b1409caaf35a036b64b2c17289
Valentin Got <gotvalentin@gmail.com>
2022-07-07 22:07:54 +0200

    chore(assets): add card type icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import Icons from 'unplugin-icons/vite';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Icons({
+      compiler: 'vue3',
+      customCollections: {
+        mz: FileSystemIconLoader('src/assets/icons')
+      }
+    }),
+    Components({
+      dts: true
+    }),
+  ]
 })
+
